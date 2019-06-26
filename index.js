c86ad438d1fde2535d0671ac962a239f02ea45af
MikeSGri <47948317+MikeSGri@users.noreply.github.com>
2019-06-26 08:42:55 -0500

    added mongodb connection

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-'use strict'
+`use strict`
 
-const express = require('express')
-const bodyParser= require('body-parser')
+const express = require(`express`)
+const bodyParser = require(`body-parser`)
+const app = express()
+const port=process.env.Port||3001
+const db=require("./db")
 
-const app=express()
-const port=process.env.PORT || 3000
-
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-app.get('/hola',(req, res)=>{
-  res.send({message:`hola Mundo!`})
+app.listen(port,()=>{
+    console.log(`hello world using localhost:${port}`)
 })
-app.listen(port,() =>{
-  console.log(`API rest in: ${port}`)
+app.get('/hola',(req,res)=>{
+    res.send({message:'hola mundo'})
+})
+
+app.get('/hola/:name',(req,res)=>{
+    res.send({message:`hola mundo2 ${req.params.name}`})
+})
+
+db.connect((err)=>{
+    if(err){
+        console.log("unable to connect to DB")
+        process.exit(1);
+    }else{
+        app.listen(port,()=>{
+            console.log("connect to db")
+        })
+    }
 })
